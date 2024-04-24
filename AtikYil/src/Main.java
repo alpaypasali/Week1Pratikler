ba7420dc0d5ef3188540af139bd58d0ee6088cf3
SifirGibi <yazilim@sifirgibimakine.com>
2024-04-24 18:21:21 +0300

    fix

---
+++ AtikYil/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bir Yıl Giriniz:");
+        int yil = scanner.nextInt();
+        boolean artik = false;
+
+        if(yil % 4 == 0)
+        {
+            if( yil % 100 == 0)
+            {
+                if ( yil % 400 == 0)
+                    artik = true;
+                else
+                    artik = false;
+            }
+            else
+                artik = true;
+        }
+        else
+            artik = false;
+
+        if(artik)
+            System.out.println(yil + " bir artık yıldır.");
+        else
+            System.out.println(yil + " bir artık yıldır değildir.");
+    }
+}
