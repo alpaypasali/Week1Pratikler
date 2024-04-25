0e4fe0ff4f1790286f177af92f87dd37d87cb155
SifirGibi <yazilim@sifirgibimakine.com>
2024-04-25 17:33:11 +0300

    sayilarinToplami

---
+++ BasamakToplami/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Bir sayı giriniz :");
+            int sayi = scanner.nextInt();
+            int toplam = 0;
+            while (sayi > 0) {
+
+                toplam += sayi % 10;
+                sayi /= 10;
+
+
+            }
+        System.out.println("Girilen sayinin rakamlarının toplamı = " + toplam);
+        }
+    }
